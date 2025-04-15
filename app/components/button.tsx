c1aa0f5bd7482deba2f0d6d40662c04d0bb3669b
marvieiradev <marcosvieira482@gmail.com>
2025-04-15 18:41:17 -0300

    feat:  add button component

---
+++ app/components/button.tsx
@@ -0,0 +1,12 @@
+const Button = (props: any) => {
+  return (
+    <button
+      type={props.type}
+      className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-lg p-4 min-w-[100px] rounded-full"
+    >
+      {props.text}
+    </button>
+  );
+};
+
+export default Button;
