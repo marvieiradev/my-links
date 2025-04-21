447eed3ee121ae13cd2c463281b4d59b44ef75ca
marvieiradev <marcosvieira482@gmail.com>
2025-04-21 20:26:59 -0300

    feat: upadate home page and login style pages

--- app/(project)/login/page.tsx
+++ app/(project)/login/page.tsx
@@ -1,6 +1,8 @@
 import { handleAuth } from "@/app/actions/handle-auth";
 import { auth } from "@/app/lib/auth";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { LogIn } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function Login() {
@@ -10,13 +12,23 @@ export default async function Login() {
   }
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-8 max-w-[800px] m-auto p-4">
-      <h1 className="text-4xl font-extrabold text-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-transparent bg-clip-text">
-        My Links
-      </h1>
-      <h2 className="text-2xl">Faça login para começar</h2>
-      <form action={handleAuth}>
-        <Button type="submit">Login</Button>
-      </form>
+      <Card className="border-2 border-indigo-500 rounded-2xl">
+        <CardContent className="flex flex-col justify-center items-center gap-8 ">
+          <h1 className="text-4xl font-extrabold text-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-transparent bg-clip-text">
+            My Links
+          </h1>
+          <h2 className="text-2xl">Bem vindo!</h2>
+          <h3 className="text-lg text-center max-w-[80%]">
+            Faça login para começar a criar seus links
+          </h3>
+          <form action={handleAuth}>
+            <Button type="submit">
+              <LogIn width={24} />
+              Faça login com o Google
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- app/(project)/page.tsx
+++ app/(project)/page.tsx
@@ -3,21 +3,34 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-8 max-w-[800px] m-auto p-4">
-      <h1 className="text-4xl font-extrabold text-center">
-        Bem-vindo ao{" "}
-        <span className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-transparent bg-clip-text">
-          My Links
-        </span>
-        !
-      </h1>
-      <h2 className="text-xl text-center">
-        Personalize seu link na bio. Um link para ajudá-lo a compartilhar tudo o
-        que você cria, seleciona e vende a partir do seu perfil de mídia social.
-      </h2>
-      <Link href="/login">
-        <Button>Quero meu link personalizado!</Button>
-      </Link>
+    <div className=" h-full w-full flex flex-col">
+      <div className="w-full mt-4 flex justify-end px-4">
+        <Link href="/login">
+          <Button>Login</Button>
+        </Link>
+      </div>
+      <div className="flex flex-col lg:flex-row justify-center itens-center px-4 w-[98%]">
+        <div className="lg:w-[50%] flex flex-col justify-center items-center gap-8 mt-6 lg:mt-0">
+          <h1 className="text-4xl font-extrabold text-center max-w-[80%]">
+            Bem-vindo ao{" "}
+            <span className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-transparent bg-clip-text">
+              My Links
+            </span>
+            !
+          </h1>
+          <h2 className="text-xl lg:text-2xl text-center max-w-[80%]">
+            Personalize seu link na bio. Um link para ajudá-lo a compartilhar
+            tudo o que você cria, seleciona e vende a partir do seu perfil de
+            mídia social.
+          </h2>
+          <Link href="/login">
+            <Button>Quero meu link personalizado!</Button>
+          </Link>
+        </div>
+        <div className="lg:w-[50%]">
+          <img src="./home.webp" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
