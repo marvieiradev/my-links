db87d1181df941d1fd886bf14dd8ca419382aec2
marvieiradev <marcosvieira482@gmail.com>
2025-04-22 19:03:37 -0300

    feat: add menu on Dashboard

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,12 @@
 "use client";
 import Link from "next/link";
 import { Card, CardContent } from "./ui/card";
-//import { Sheet, SheetTrigger } from "./ui/sheet";
-//import { MenuIcon } from "lucide-react";
-//import { Button } from "./ui/button";
 import Image from "next/image";
 import { Button } from "./ui/button";
-//import { MenuIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 import { handleAuth } from "@/app/actions/handle-auth";
-//import SidebarSheet from "./sidebar-sheet";
+import { LogOut, MenuIcon, Wallet } from "lucide-react";
+import { useState } from "react";
 
 interface HeaderProps {
   image: string;
@@ -18,6 +15,8 @@ interface HeaderProps {
 const Header = (props: HeaderProps) => {
   const handleLogoutClick = () => handleAuth();
   const toPayments = () => redirect("/payments");
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
 
   return (
     <>
@@ -26,37 +25,51 @@ const Header = (props: HeaderProps) => {
           <Link href="/">
             <Image src="/my-logo.svg" height={26} width={26} alt="logo" />
           </Link>
-          <p className="hidden md:flex">Dashboard</p>
+          <p>Dashboard</p>
 
           <div className="flex justify-center gap-2">
-            <Button onClick={toPayments}>Planos</Button>
             <Button
               variant="outline"
-              className="min-w-0 font-semibold"
-              onClick={handleLogoutClick}
+              className="min-w-0"
+              onClick={handleMenuClick}
             >
               <img
                 className="w-[30px] h-[30px] rounded-full"
                 src={props.image}
                 alt="avatar"
               />
-              LogOut
+              <MenuIcon />
             </Button>
-          </div>
 
-          {/*<Sheet>
-            <SheetTrigger asChild>
-              <Button variant="outline" className="min-w-0">
-                <img
-                  className="w-[30px] h-[30px] rounded-full"
-                  src={props.image}
-                  alt="avatar"
-                />
-                <MenuIcon />
-              </Button>
-            </SheetTrigger>
-            <SidebarSheet />
-          </Sheet>*/}
+            {menuIsOpen && (
+              <div
+                className="z-50 absolute top-12 right-6 w-[100px] h-[70px] bg-white shadow-md flex flex-col justify-center items-start"
+                onMouseLeave={() => setMenuIsOpen(false)}
+              >
+                <Link href="/my-trips" onClick={() => setMenuIsOpen(false)}>
+                  <button
+                    className="text-indigo-500 text-base font-semibold cursor-pointer"
+                    onClick={toPayments}
+                  >
+                    <div className="flex flex-row items-center gap-2 border-b border-background w-[100px] h-[35px] p-1 hover:bg-indigo-500 hover:text-white">
+                      <Wallet size={20} />
+                      Planos
+                    </div>
+                  </button>
+                </Link>
+
+                <button
+                  className="text-indigo-500 text-base font-semibold cursor-pointer"
+                  onClick={handleLogoutClick}
+                >
+                  <div className="flex flex-row items-center gap-2 w-[100px] h-[35px] p-1 hover:bg-indigo-500 hover:text-white">
+                    <LogOut size={20} />
+                    Logout
+                  </div>
+                </button>
+              </div>
+            )}
+          </div>
         </CardContent>
       </Card>
     </>
